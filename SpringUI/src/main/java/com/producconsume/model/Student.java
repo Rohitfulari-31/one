1fc7e6fc511cea90c421b02c07a01f60d9caa325
rohitfulari-31 <rohitfulari@gmail.com>
2021-01-04 19:41:11 +0530

    2 commited

---
+++ SpringUI/src/main/java/com/producconsume/model/Student.java
@@ -0,0 +1,38 @@
+package com.producconsume.model;
+
+public class Student 
+{
+
+	private String name;
+	private String mobileno;
+	private String uname;
+	private String password;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMobileno() {
+		return mobileno;
+	}
+	public void setMobileno(String mobileno) {
+		this.mobileno = mobileno;
+	}
+	public String getUname() {
+		return uname;
+	}
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	
+	
+}
+
